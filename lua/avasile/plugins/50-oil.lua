91f15bfd019005a0a6fa9233a0ffd85bdd1aeeed
Andrei Vasile <andrei@avasile.com>
2026-01-19 17:25:55 +0000

    setup oil + nvim-tree

--- lua/avasile/plugins/50-oil.lua
+++ lua/avasile/plugins/50-oil.lua
@@ -1,23 +1,68 @@
+-- lazy.nvim plugin entries
 return {
+	-- nvim-tree (sidebar)
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			hijack_netrw = false, -- don't fight netrw/Oil
+			open_on_tab = false,
+			update_focused_file = {
+				enable = false, -- avoid surprise focus changes
+			},
+			view = {
+				width = 30,
+				side = "left",
+				preserve_window_proportions = true,
+			},
+			renderer = {
+				icons = { show = { file = true, folder = true, git = true } },
+			},
+			actions = { open_file = { quit_on_open = false } }, -- keep tree open if desired
+		},
+		keys = {
+			{
+				"<leader>T",
+				":NvimTreeToggle<CR>",
+				noremap = true,
+				silent = true,
+				desc = "Toggle nvim-tree sidebar",
+			},
+		},
+	},
+
+	-- Oil (directory-as-buffer)
 	{
 		"stevearc/oil.nvim",
-		---@module 'oil'
-		---@type oil.SetupOpts
-		opts = {
-			default_file_explorer = true,
-			columns = {
-				"icon",
-				"size",
-				"permissions",
-				"mtime",
-			},
-			delete_to_trash = true,
-			watch_for_changes = true,
-		},
-		-- Optional dependencies
-		-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-		lazy = false,
+		config = function()
+			require("oil").setup({
+				-- columns = { "icon", "name", "size", "mtime" },
+				use_default_keymaps = false, -- we'll set our own to avoid collisions
+				view_options = { show_hidden = true },
+			})
+
+			-- dedicated keymap to open Oil (explicit action)
+			vim.keymap.set(
+				"n",
+				"<leader>e",
+				"<cmd>Oil<CR>",
+				{ noremap = true, silent = true, desc = "Open Oil (dir buffer)" }
+			)
+
+			-- Buffer-local mappings for Oil buffers (safer than global maps)
+			-- vim.api.nvim_create_autocmd("FileType", {
+			-- 	pattern = "oil",
+			-- 	callback = function()
+			-- 		local opts = { buffer = true, noremap = true, silent = true }
+			-- 		local oil = require("oil")
+			-- 		-- navigation like a file explorer
+			-- 		-- vim.keymap.set("n", "h", oil.open_parent, opts)
+			-- 		-- vim.keymap.set("n", "l", oil.open, opts)
+			-- 		-- vim.keymap.set("n", "<CR>", oil.open, opts)
+			-- 		-- vim.keymap.set("n", "r", oil.refresh, opts)
+			-- 		-- vim.keymap.set("n", "d", oil.remove, opts)
+			-- 	end,
+			-- })
+		end,
 	},
 }
